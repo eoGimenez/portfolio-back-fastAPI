3043f15f36576b895eac46b05df0e19208f39bb8
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-04 19:02:31 +0200

    soluciones2

--- schemas/projects.py
+++ schemas/projects.py
@@ -16,32 +16,3 @@ class Project(BaseModel):
     urlGit: list[dict] = Field(default_factory=list)
     image: Optional[HttpUrl]
     author: Optional[dict]
-
-
-# def project_schema(projects) -> list:
-#     return [project == Project(**project) for project in projects]
-
-
-""" 
-PARA ENVIAR PETICIONES DE PRUEBA A LA API
-
-{
-    "title": "test",
-    "description": "descripcion",
-    "secDescription": "segunda des",
-    "technologies": ["PROBANDO", "QUE ONDA", "akka"],
-    "urlGit": [
-      {
-       "label": "titulo aaaaaaaaaaaimagen",
-       "url": "https://www.google.com"
-        
-      },
-         {
-       "label": "titul SEGUssssssssNDA imagen",
-       "url": "https://www.youtube.com"
-        
-      }
-        ],
-    "image": "https://res.cloudinary.com/dbld4vcec/image/upload/v1685561602/projects/vab1fxizdea2tryposca.png",
-    "author": {"email": "Eugeni"}
-} """
